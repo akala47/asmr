b62b8c41154f9362de3e0fb43ee8e2cc317125e1
akala47 <kalaskar.a@northeastern.edu>
2025-03-13 01:00:58 -0400

    Added initial example for macro exapansion

--- asmr_use.rkt
+++ asmr_use.rkt
@@ -1,24 +1,24 @@
-;;NOTE: For Racket Operations
-#lang racket
+;;; ;;NOTE: For Racket Operations
+;;; #lang racket
 
-(require asmr/asm-block)
+;;; (require asmr/asm-block)
 
-(asm-block
-  (registers [(rax 0) (rbx 0) (rcx 0)])
-  [(cmp rax rbx)
-   (jne error)
-   (mov rcx 10)
-   (jmp exit)
-   (label error)
-   (mov rax 10)
-   (label exit)
-   (print-registers)])
+;;; (asm-block
+;;;   (registers [(rax 0) (rbx 0) (rcx 0)])
+;;;   [(cmp rax rbx)
+;;;    (jne error)
+;;;    (mov rcx 10)
+;;;    (jmp exit)
+;;;    (label error)
+;;;    (mov rax 10)
+;;;    (label exit)
+;;;    (print-registers)])
 
-;;____EXPECTED OUTPUT____
+;;; ;;____EXPECTED OUTPUT____
 
-;;| Register : Value |
-;;+------------+--------+
-;;| 'rax : 0
-;;| 'rbx : 0 
-;;| 'rcx : 10
-;;+------------+--------+
+;;; ;;| Register : Value |
+;;; ;;+------------+--------+
+;;; ;;| 'rax : 0
+;;; ;;| 'rbx : 0 
+;;; ;;| 'rcx : 10
+;;; ;;+------------+--------+
---
+++ asmr_use_racketcode.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+;; Note: Initial version only used to build support for a few basic operations.
+
+(begin
+  (define registers
+    (make-hash 
+          '(["rax" . 0]
+            ["rbx" . 0]
+            ["rcx" . 0])))
+
+  (displayln "Initial registers:")
+  (for ([(k v) (in-hash registers)])
+    (displayln (format "  ~a => ~a" k v)))
+  (newline)
+
+  (begin
+    (define var (hash-ref registers "rcx" "Error"))
+
+    (hash-set*! registers 
+                "rcx" 
+                ((lambda (x) (+ x 10)) var)))
+
+  (for ([(k v) (in-hash registers)])
+    (displayln (format "  ~a => ~a" k v)))
+  (newline)
+
+
+  (displayln "----FINAL REGISTERS---")
+  (for ([(k v) (in-hash registers)])
+    (displayln (format "  ~a => ~a" k v))))
