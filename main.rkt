f71e78d2ab52dc6fde2138972c51958a900aaa95
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2025-04-02 15:43:03 -0400

    globalololol

--- main.rkt
+++ main.rkt
@@ -1,89 +1,84 @@
 #lang racket
 
 (require syntax-spec-v3
-         (for-syntax syntax/parse racket))
+         (for-syntax syntax/parse racket/list))
 
 (syntax-spec
- ;; Binding class for registers
- ;(binding-class register)
-
+ (binding-class register)
+ (binding-class label)
+ 
  (host-interface/expression
-  (asm-block inst:instruction-spec ...)
-  #:binding (inst ...)
-  #'(compile-asm-instructions inst ...))
-
+  (asm-block registers:registers-spec [inst:instruction-spec ...])
+  #:binding (scope (import registers) (import inst) ...)
+  #'(compile-asm-instructions registers [inst ...])) ;; Compile to Racket using bound variables
+  
+ (nonterminal/exporting registers-spec
+                        (registers ([reg-name:register e:arith-expr] ...))
+                        #:binding ((export reg-name) ...))
 
  (nonterminal arith-expr
-   n:number
-   e:racket-expr)
+              n:number
+              (+ e1:arith-expr e2:arith-expr)
+              (- e1:arith-expr e2:arith-expr)
+              (* e1:arith-expr e2:arith-expr))
+  
+ (nonterminal/exporting instruction-spec
+                        (label lbl:label)
+                        #:binding (export lbl) ;; Note: We want to export the label from our nonterminal to machine host interface 
+                        (print-registers)
+                        (cmp reg1:register reg2:register) ;; Instruction syntax not yet implemented
+                        (jne lbl:label)
+                        (mov reg:register val:arith-expr)
+                        (jmp lbl:label)))
 
- 
- (nonterminal/exporting register
-   r:racket-var
-   #:binding (export r))
-
- 
- (nonterminal instruction-spec
-   (cmp reg1:register reg2:register)
-   #:binding (scope(import reg1) (import reg2))  ; Import registers
-
-   (mov reg:register val:arith-expr)
-   #:binding (scope (import reg) val)  ; Import register
-
-   (print-registers)))
-
-;; TODO
-
-;; Use a hash table to store registers
-(define registers (make-hash))
-
-
-
-;; Runtime functions for register operations
-(define (register-set! reg val) (hash-set! registers reg val))
-(define (register-get reg) (hash-ref registers reg #f))
-(define (display-registers) (displayln registers))
-
-(define (add-64-registers)
-  (begin (register-set! 'rax 0)
-         (register-set! 'rbx 0)
-         (register-set! 'rcx 0)
-         (register-set! 'rdx 0)
-         (register-set! 'r8 0)
-         (register-set! 'r9 0)))
+(define (register-set! map reg val) (hash-set! map reg val))
+(define (register-get map reg) (hash-ref map reg #f))
+(define (display-registers map) (displayln map))
+(define registers-map (make-hash))
 (define compare-flag #f)
 
+(define (compare v1 v2)
+  (begin
+    (if (or (false? v1)
+            (false? v2))
+        (error "Register not valid")
+        (if (eq? v1 v2)
+            (set! compare-flag #t)
+            (set! compare-flag #f)))))
 
-;; Macro to expand instructions without using `define`
 (define-syntax compile-asm-instructions
   (syntax-parser
-    [(_ inst ...)
+    [(_ registers [instrs ...])
      #`(begin
-           (add-64-registers)
-           #,@(for/list ([instr (syntax->list #'(inst ...))])
+         (let* ([registers-list (cadr (syntax->datum #'registers))])
+           
+           (for ([reg registers-list])
+             (let ([reg-name (first reg)] 
+                   [reg-value (second reg)])
+               (register-set! registers-map reg-name reg-value)))
+           
+           #,@(for/list ([instr (syntax->list #'(instrs ...))])
                 (syntax-parse instr
-                  [(mov reg val) #'(register-set! 'reg val)]
-                  [(cmp reg1 reg2) #'(compare (register-get 'reg1) (register-get 'reg2))]
-                  [(print-registers) #'(display-registers)])))]))
+                  [((~datum cmp) reg1 reg2)
+                   #`(let* ([val1 (register-get registers-map 'reg1)]
+                            [val2 (register-get registers-map 'reg2)])
+                       (compare val1 val2))]
+                  [((~datum jne) lbl)
+                   #'(displayln "2. Jump if not equal")]
+                  [((~datum mov) reg val)
+                   #`(register-set! registers-map 'reg val)]
+                  [((~datum jmp) lbl)
+                   #'(displayln "4. Unconditional jump")]
+                  [((~datum label) lbl)
+                   #'(displayln "5. Label defined")]
+                  [(print-registers) #`(display-registers registers-map)]
+                  [_ #'(displayln "7. Unknown")]))
 
-(define (compare val1 val2)
-  (begin (if (or (false? val1 #f)
-                 (false? val2 #f))
-             (error "Register not valid")
-             (if (eq? val1 val2)
-                 (set! compare-flag #t)
-                 (set! compare-flag #f)) )))
-
-;; Example usage
-(asm-block
- (mov rax 10)
- (mov rbx 20)
- (cmp rax rbx)
- (print-registers))
+           (void)))]))
 
 
 (asm-block
- (mov rax 20)
- (mov rbx 10)
- ;(cmp rzx rbx)
- (print-registers))
+ (registers [(rax 0) (rbx 10) (rcx 0)])
+ [(mov rax 10)
+  (cmp rax rbx)
+  (print-registers)])
