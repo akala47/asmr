da535da20a8d408a1840fee8855a4ad2d1316d84
akala47 <kalaskar.a@northeastern.edu>
2025-04-02 13:27:16 -0400

    cmp template

--- asmr_use.rkt
+++ asmr_use.rkt
@@ -1,18 +1,19 @@
-#lang racket
-
 ;; NOTE: For Racket Operations
+
+#lang racket
 
 (require asmr/asm-block)
 
 (asm-block
-   (cmp rax rbx)
+  (registers [(rax 0) (rbx 0) (rcx 0)])
+  [(cmp rax rbx)
    (jne error)
    (mov rcx 10)
    (jmp exit)
    (label error)
    (mov rax 10)
    (label exit)
-   (print-registers))
+   (print-registers)])
 
 ;;____EXPECTED OUTPUT____
 
@@ -23,3 +24,15 @@
 ;; | 'rbx       : 0      |
 ;; | 'rcx       : 10     |
 ;; +------------+--------+
+
+
+(asm-block
+  (registers [(rax 0) (rbx 0) (rcx 0)])
+  [(cmp rax rbx) 
+   (jne error)
+   (mov rcx 10)
+   (jmp exit)
+   (label error)
+   (mov rax 10)
+   (label exit)
+   (print-registers)])
--- main.rkt
+++ main.rkt
@@ -5,7 +5,7 @@
 
 (syntax-spec
  ;; Binding class for registers
- (binding-class register)
+ ;(binding-class register)
 
  (host-interface/expression
   (asm-block inst:instruction-spec ...)
@@ -18,25 +18,61 @@
    e:racket-expr)
 
  
- (nonterminal/exporting register-spec
-   r:register
+ (nonterminal/exporting register
+   r:racket-var
    #:binding (export r))
 
  
  (nonterminal instruction-spec
-   (cmp reg1:register-spec reg2:register-spec)
+   (cmp reg1:register reg2:register)
    #:binding (scope(import reg1) (import reg2))  ; Import registers
 
-   (mov reg:register-spec val:arith-expr)
+   (mov reg:register val:arith-expr)
    #:binding (scope (import reg) val)  ; Import register
 
    (print-registers)))
 
 ;; TODO
+
+;; Use a hash table to store registers
+(define registers (make-hash))
+
+
+
+;; Runtime functions for register operations
+(define (register-set! reg val) (hash-set! registers reg val))
+(define (register-get reg) (hash-ref registers reg #f))
+(define (display-registers) (displayln registers))
+
+(define (add-64-registers)
+  (begin (register-set! 'rax 0)
+         (register-set! 'rbx 0)
+         (register-set! 'rcx 0)
+         (register-set! 'rdx 0)
+         (register-set! 'r8 0)
+         (register-set! 'r9 0)))
+(define compare-flag #f)
+
+
+;; Macro to expand instructions without using `define`
 (define-syntax compile-asm-instructions
   (syntax-parser
     [(_ inst ...)
-     #'(length `(syntax->list inst ...))]))
+     #`(begin
+           (add-64-registers)
+           #,@(for/list ([instr (syntax->list #'(inst ...))])
+                (syntax-parse instr
+                  [(mov reg val) #'(register-set! 'reg val)]
+                  [(cmp reg1 reg2) #'(compare (register-get 'reg1) (register-get 'reg2))]
+                  [(print-registers) #'(display-registers)])))]))
+
+(define (compare val1 val2)
+  (begin (if (or (false? val1 #f)
+                 (false? val2 #f))
+             (error "Register not valid")
+             (if (eq? val1 val2)
+                 (set! compare-flag #t)
+                 (set! compare-flag #f)) )))
 
 ;; Example usage
 (asm-block
@@ -45,9 +81,9 @@
  (cmp rax rbx)
  (print-registers))
 
+
 (asm-block
- (mov rax 10)
- (mov rbx 20)
- (cmp rax rbx)
- (mov rax 10)
+ (mov rax 20)
+ (mov rbx 10)
+ ;(cmp rzx rbx)
  (print-registers))
