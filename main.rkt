78b60ad5039f5f2299ca1f8cfbdd4b80f00b6b60
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2025-04-02 16:08:40 -0400

    full program

--- main.rkt
+++ main.rkt
@@ -78,7 +78,12 @@
 
 
 (asm-block
- (registers [(rax 0) (rbx 10) (rcx 0)])
- [(mov rax 10)
-  (cmp rax rbx)
-  (print-registers)])
+  (registers [(rax 0) (rbx 0) (rcx 0)])
+  [(cmp rax rbx)        ; 0
+   (jne error)          ; 1
+   (mov rcx 10)         ; 2
+   (jmp exit)           ; 3
+   (label error)        ; 4
+   (mov rax 10)         ; 5
+   (label exit)         ; 6
+   (print-registers)])  ; 7
